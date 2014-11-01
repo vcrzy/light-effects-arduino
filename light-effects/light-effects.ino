9f74b954af0e2b0d97beac6114afdfeaf704e413
Vinícius Alves <mrvini@live.com>
2014-11-01 00:46:22 -0200

    Update light-effects.ino

--- light-effects/light-effects.ino
+++ light-effects/light-effects.ino
@@ -1,37 +1,37 @@
 byte leds[] = {4,5,6,7,8,9,10,11,12,13};
 int ledDelay;
-int direcao = 1;
-int ledAtual = 0;
+int direction = 1;
+int currentLed = 0;
 int potPin = 3;
-unsigned long mudarTempo;
+unsigned long timeChange;
 
 void setup(){
   for(int x = 0; x < 10; x++){
     pinMode(leds[x],OUTPUT);
   }
-  mudarTempo = millis();
+  timeChange = millis();
   Serial.begin(9600);
 }
 
 void loop(){
   ledDelay = analogRead(potPin);
-   Serial.println(analogRead(potPin));
+  Serial.println(analogRead(potPin));
   
-  if((millis() - mudarTempo) > ledDelay){
-    mudarLed();
-    mudarTempo = millis();
+  if((millis() - timeChange) > ledDelay){
+    ledChange();
+    timeChange = millis();
   }
 }
 
-void mudarLed(){
+void ledChange(){
   for(int x = 0; x < 10; x++){
     digitalWrite(leds[x], LOW);
   }
   
-  digitalWrite(leds[ledAtual],HIGH);
-  ledAtual += direcao;
-  if(ledAtual == 9){ direcao = -1;}
-  if(ledAtual == 0){ direcao = 1;}
+  digitalWrite(leds[currentLed],HIGH);
+  currentLed += direction;
+  if(currentLed == 9){ direction = -1;}
+  if(currentLed == 0){ direction = 1;}
 }
 
 
